b7138aab9ea1e6fda3739fdab5d47db4a7e2c7f7
dj <d.dj@foxmail.com>
2025-04-14 22:03:28 +0800

    lc 11

---
+++ src/11-盛水最多的容器.test.ts
@@ -0,0 +1,29 @@
+import { expect, test } from 'vitest';
+
+/**
+ * 思路: 左右双指针
+ * - 优先移动短板指针
+ */
+function maxArea(height: number[]): number {
+  let area = 0;
+
+  let p1 = 0;
+  let p2 = height.length - 1;
+
+  while (p1 < p2) {
+    area = Math.max(area, Math.min(height[p1], height[p2]) * (p2 - p1));
+
+    if (height[p1] < height[p2]) {
+      p1++;
+    } else {
+      p2--;
+    }
+  }
+
+  return area;
+}
+
+test('maxArea', () => {
+  expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toEqual(49);
+  expect(maxArea([1, 1])).toEqual(1);
+});
